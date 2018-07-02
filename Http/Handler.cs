3e9d40e273a80487b143898d0541a964342466f5
Quynh Nguyen <mr.quynh@gmail.com>
2018-07-02 13:36:28 +0700

    Add health-check for working with load balancing

--- Http/Handler.cs
+++ Http/Handler.cs
@@ -40,10 +40,10 @@ namespace net.vieapps.Services.APIGateway
 			// request of HTTP
 			else
 			{
-				// allow origin
+				// CORS: allow origin
 				context.Response.Headers["Access-Control-Allow-Origin"] = "*";
 
-				// request with OPTIONS verb
+				// CORS: options
 				if (context.Request.Method.IsEquals("OPTIONS"))
 				{
 					var headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
@@ -55,20 +55,27 @@ namespace net.vieapps.Services.APIGateway
 					context.SetResponseHeaders((int)HttpStatusCode.OK, headers, true);
 				}
 
-				// request with other verbs
-				else
-					await this.ProcessRequestAsync(context).ConfigureAwait(false);
-			}
+				// load balancing health check
+				else if (context.Request.Path.Value.IsEquals("/load-balancing-health-check"))
+					await context.WriteAsync("OK", "text/plain", null, 0, null, TimeSpan.Zero, null, Global.CancellationTokenSource.Token).ConfigureAwait(false);
 
-			// invoke next middleware
-			try
-			{
-				await this.Next.Invoke(context).ConfigureAwait(false);
-			}
-			catch (InvalidOperationException) { }
-			catch (Exception ex)
-			{
-				Global.Logger.LogCritical($"Error occurred while invoking the next middleware: {ex.Message}", ex);
+				// request of APIs
+				else
+				{
+					// process
+					await this.ProcessRequestAsync(context).ConfigureAwait(false);
+
+					// invoke next middleware
+					try
+					{
+						await this.Next.Invoke(context).ConfigureAwait(false);
+					}
+					catch (InvalidOperationException) { }
+					catch (Exception ex)
+					{
+						Global.Logger.LogCritical($"Error occurred while invoking the next middleware: {ex.Message}", ex);
+					}
+				}
 			}
 		}
 
--- Http/InternalAPIs.cs
+++ Http/InternalAPIs.cs
@@ -29,7 +29,7 @@ namespace net.vieapps.Services.APIGateway
 		#region Properties
 		internal static ICache Cache { get; set; }
 		internal static ILogger Logger { get; set; }
-		internal static List<string> ExcludedHeaders { get; } = "connection,accept,accept-encoding,accept-language,cache-control,cookie,content-type,content-length,user-agent,referer,host,origin,if-modified-since,if-none-match,upgrade-insecure-requests,ms-aspnetcore-token,x-original-proto,x-original-for,x-original-remote-endpoint".ToList();
+		internal static List<string> ExcludedHeaders { get; } = "connection,accept,accept-encoding,accept-language,cache-control,cookie,content-type,content-length,user-agent,referer,host,origin,if-modified-since,if-none-match,upgrade-insecure-requests,ms-aspnetcore-token,x-forwarded-for,x-forwarded-proto,x-forwarded-port,x-original-for,x-original-proto,x-original-remote-endpoint".ToList();
 		internal static HashSet<string> NoTokenRequiredServices { get; } = (UtilityService.GetAppSetting("NoTokenRequiredServices", "") + "|indexes|discovery").ToLower().ToHashSet('|', true);
 		internal static ConcurrentDictionary<string, JObject> Controllers { get; } = new ConcurrentDictionary<string, JObject>();
 		internal static ConcurrentDictionary<string, List<JObject>> Services { get; } = new ConcurrentDictionary<string, List<JObject>>();

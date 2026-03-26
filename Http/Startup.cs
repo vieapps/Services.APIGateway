6d0303e3c5002cd0129d30f3454793baf4608a0e
Quynh Nguyen <mr.quynh@gmail.com>
2026-03-26 17:21:37 +0700

    (improv): monitors' logs

--- Http/Startup.cs
+++ Http/Startup.cs
@@ -298,6 +298,7 @@ namespace net.vieapps.Services.APIGateway
 				Global.Logger.LogInformation($"The {Global.ServiceName} HTTP service was started - PID: {Environment.ProcessId} - Execution times: {stopwatch.GetElapsedTimes()}");
 				Global.Logger = loggerFactory.CreateLogger<Handler>();
 
+				Global.MonitorLogFilePattern = "{service}.http-{hour}-monitor.txt";
 				Global.StartMonitor(logPath);
 			});
 
